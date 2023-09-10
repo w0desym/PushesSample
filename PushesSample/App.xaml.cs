ab6ff410b635332ad1c4aa0acca047c5435db626
Illia Andrieiev <mr.andreev2012@gmail.com>
2023-09-10 18:22:14 +0300

    create solution

---
+++ PushesSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PushesSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ PushesSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PushesSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
